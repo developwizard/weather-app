382f0a402d1fa26c61c16d1e3b166e7e62bce0e3
dewiz <churkin.it@gmail.com>
2023-07-08 22:26:20 +0300

    Added environment variable

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'd39aab8ac3mshc453772cfb4a79ap1e6dd1jsn01e1d0171197',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com'
+};
