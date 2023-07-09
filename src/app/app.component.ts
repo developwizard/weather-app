f3eb61aa3eaefac7c149553aeda1174451aeefde
dewiz <churkin.it@gmail.com>
2023-07-09 20:48:56 +0300

    Added response data to template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {WeatherService} from "./services/weather.service";
+import {WeatherData} from "./models/weather.model";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'weather-app';
+export class AppComponent implements OnInit {
+  weatherData?: WeatherData;
+
+  constructor(private weatherService: WeatherService) {
+  }
+
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('London')
+      .subscribe({
+        next: (response) => {
+          this.weatherData = response;
+          console.log(this.weatherData);
+        }
+      });
+  }
 }
