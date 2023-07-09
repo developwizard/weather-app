4d98f296033440c266c858d63830d78aa663e357
dewiz <churkin.it@gmail.com>
2023-07-09 20:41:16 +0300

    Fix: environment data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com',
+  weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/',
   XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
   XRapidAPIKeyHeaderValue: 'd39aab8ac3mshc453772cfb4a79ap1e6dd1jsn01e1d0171197',
   XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
